5262b26f5e7b57b031d120d7ee7a697a1634d2ba
Sanjay ng <106653066+Sanjayng125@users.noreply.github.com>
2024-05-18 19:09:31 +0530

    Unnecessary style changes

--- src/components/search/Search.tsx
+++ src/components/search/Search.tsx
@@ -17,14 +17,14 @@ const Search = () => {
     <div className="flex items-center gap-2">
       <input
         type="text"
-        className="rounded-full p-2 outline-none bg-white bg-opacity-50 w-full border-[6px] border-purple-400 dark:border-white dark:border-opacity-10 dark:bg-transparent text-black dark:text-white"
+        className="rounded-full p-2 outline-none bg-white bg-opacity-50 w-full border-[6px] border-purple-400 dark:border-white dark:border-opacity-30 dark:bg-transparent text-black dark:text-white"
         placeholder="Search"
         value={searchInput}
         onChange={(e) => setSearchInput(e.target.value)}
       />
       <Link
         href={searchInput !== "" ? `/search?q=${searchInput}` : "/explore"}
-        className="bg-purple-400 rounded-full p-2 text-2xl text-white hover:scale-95 dark:bg-white dark:bg-opacity-10"
+        className="bg-purple-400 rounded-full p-2 text-2xl text-white hover:scale-95 dark:bg-white dark:bg-opacity-30"
       >
         <FaSearch />
       </Link>
