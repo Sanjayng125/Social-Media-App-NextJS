164bdcebae952bd102cf10d1802e2731cd869119
Sanjay ng <106653066+Sanjayng125@users.noreply.github.com>
2024-05-18 18:52:45 +0530

    Small style change

--- src/components/search/Search.tsx
+++ src/components/search/Search.tsx
@@ -17,7 +17,7 @@ const Search = () => {
     <div className="flex items-center gap-2">
       <input
         type="text"
-        className="rounded-full p-2 outline-none bg-white bg-opacity-50 w-full border-[6px] border-purple-400 dark:border-white dark:bg-transparent text-black dark:text-white"
+        className="rounded-full p-2 outline-none bg-white bg-opacity-50 w-full border-[6px] border-purple-400 dark:border-white dark:border-opacity-10 dark:bg-transparent text-black dark:text-white"
         placeholder="Search"
         value={searchInput}
         onChange={(e) => setSearchInput(e.target.value)}
