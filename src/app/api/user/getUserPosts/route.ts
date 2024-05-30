ba68fedf58f62387cd66c0effa9ccda02404ac2c
Sanjayng <sanjayng001@gmail.com>
2024-05-30 10:16:53 +0530

    posts sort

--- src/app/api/user/getUserPosts/route.ts
+++ src/app/api/user/getUserPosts/route.ts
@@ -1,28 +1,62 @@
-import { auth } from "@/lib/auth"
+import { auth } from "@/lib/auth";
 import { connectToDb } from "@/lib/db";
-import { User } from "@/lib/models";
+import { User, Post } from "@/lib/models";
+import { imageProps } from "@/types";
+
+export interface Post {
+    _id: string
+    caption: string;
+    createdAt: any
+    createdBy: {
+        username: string
+        avatar: {
+            url: string
+        }
+        _id: string
+    };
+    images: imageProps[]
+    likes: string[]
+    tags: string[]
+}
 
 export const GET = async () => {
-    const session = await auth()
+    const session = await auth();
 
     if (session && session?.user) {
         try {
-            await connectToDb()
+            await connectToDb();
 
-            const userPosts = await User.findById(session?.user?.id).populate("posts likedPosts").select("posts likedPosts").sort({ "createdAt": -1 })
+            // Fetch the user with populated posts and likedPosts
+            const user = await User.findById(session?.user?.id).populate("posts likedPosts").select("posts likedPosts");
 
-            if (userPosts) {
-                return Response.json({ userPosts })
+            if (user) {
+                // Type assertion to ensure TypeScript understands the type
+                const sortedPosts = (user.posts as Post[]).sort((a, b) => {
+                    const dateA = new Date(a.createdAt).getTime();
+                    const dateB = new Date(b.createdAt).getTime();
+                    return dateB - dateA;
+                });
+
+                user.posts = sortedPosts
+
+                // Return the user posts with the sorted posts
+                return new Response(JSON.stringify({ userPosts: user }), {
+                    headers: { 'Content-Type': 'application/json' },
+                });
             }
-            return Response.json({ userPosts: [] })
+            return new Response(JSON.stringify({ userPosts: [] }), {
+                headers: { 'Content-Type': 'application/json' },
+            });
 
         } catch (error) {
             console.log(error);
-            return Response.json({ userPosts: [] })
+            return new Response(JSON.stringify({ userPosts: [] }), {
+                headers: { 'Content-Type': 'application/json' },
+            });
         }
     } else {
         return new Response("Forbidden", {
             status: 403
-        })
+        });
     }
-}
+};
